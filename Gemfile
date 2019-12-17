b84100e607e7d2cbd263816f729668368879ace8
LimitToInfinity <21stLimitless@gmail.com>
2019-12-17 01:10:21 -0700

    Adds authentication controller and login route/action. Also adds test for checking for authorization on people index.

--- Gemfile
+++ Gemfile
@@ -41,3 +41,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'fast_jsonapi', '~> 1.5'
+
+gem "jwt", "~> 2.2"
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,8 +3,19 @@ class PeopleController < ApplicationController
     before_action :find_person, only: [:show, :update, :destroy]
 
     def index
+        authorization_header = request.headers[:authorization]
+        if (!authorization_header)
+            render status: :unauthorized
+        else
+            token = authorization_header.split(" ")[1]
+            secret_key = Rails.application.secrets.secret_key_base[0]
+            decoded_token = JWT.decode(token, secret_key)
+
+            @user = User.find(decoded_token[0]["user_id"])
+
         @people = Person.all
         render json: PersonSerializer.new(@people)
+        end
     end
 
     def show
