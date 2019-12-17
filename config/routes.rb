18fe3d409a96b95bba55c4879b7eff88b804a732
LimitToInfinity <21stLimitless@gmail.com>
2019-12-17 00:37:55 -0700

    Added User model, migration, controller with create, and resource for create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :people
+  resources :people, only: [:index, :update]
   resources :flights
   resources :rides
   resources :shuttles
+  resources :users, only: [:create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_190942) do
+ActiveRecord::Schema.define(version: 2019_12_17_072237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2019_11_30_190942) do
 
   create_table "shuttles", force: :cascade do |t|
     t.datetime "datetime"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
