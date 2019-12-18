a0b8819997ea867e32af971cb0891c8dfb186340
LimitToInfinity <21stLimitless@gmail.com>
2019-12-18 00:45:17 -0700

    puts username and id for testing.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -10,12 +10,15 @@ class AuthenticationController < ApplicationController
         puts 4
         puts 5
         puts @user
+        puts @user.id
+        puts @user.username
 
         if (!@user)
             render status: :unauthorized
         else
             if (@user.authenticate(params[:password]))
                 secret_key = Rails.application.credentials.dig(:secret_key_base)
+                puts secret_key
                 # secret_key = Rails.application.secrets.secret_key_base[0]
                 token = JWT.encode({
                     user_id: @user.id,
