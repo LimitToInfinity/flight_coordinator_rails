c771aaaff817634ece12699a7f254dbf62e0bee5
LimitToInfinity <21stLimitless@gmail.com>
2019-12-18 00:52:42 -0700

    Removes all puts and pull secret_key from heroku env config vars.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,23 +3,12 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by(username: params[:username])
 
-        puts params[:username]
-        puts 1
-        puts 2
-        puts 3
-        puts 4
-        puts 5
-        puts @user
-        puts @user.id
-        puts @user.username
-
         if (!@user)
             render status: :unauthorized
         else
             if (@user.authenticate(params[:password]))
-                secret_key = Rails.application.credentials.dig(:secret_key_base)
-                puts secret_key
                 # secret_key = Rails.application.secrets.secret_key_base[0]
+                secret_key = ENV['SECRET_KEY_BASE']
                 token = JWT.encode({
                     user_id: @user.id,
                     username: @user.username,
