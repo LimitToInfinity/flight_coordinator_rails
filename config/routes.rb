1e17b732eb8d557f72a1c49a4f6f2d7d5e426338
LimitToInfinity <21stLimitless@gmail.com>
2021-08-01 20:15:43 -0600

    Adds user create route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :flights
   resources :rides
   resources :shuttles
-  # resources :users, only: :create
+  resources :users, only: :create
 
   post 'login', to: 'authentication#login'
 end
