9aa14b798410c397e0120c46e9bab7e792bf8eb6
LimitToInfinity <21stLimitless@gmail.com>
2021-08-01 20:19:08 -0600

    Removes user create route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :flights
   resources :rides
   resources :shuttles
-  resources :users, only: :create
 
   post 'login', to: 'authentication#login'
 end
