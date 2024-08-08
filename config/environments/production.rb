71b253b74e72a7018e5bb19c818650afceb0347d
Ahmed <21stLimitless@gmail.com>
2024-08-09 06:23:47 +0800

    Allow back4app permissions to config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Allow Permissions
+  config.hosts << 'flightcoordinatorbe-dlw2ltwz.b4a.run'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
